e5ff0e862f2eabb4e22b857a052e33406c9465ff
Enki <104570540+Thierry-75@users.noreply.github.com>
2024-06-24 11:54:50 +0200

    method add avatar

--- src/Controller/AvatarController.php
+++ src/Controller/AvatarController.php
@@ -2,15 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Avatar;
+use App\Service\ImageService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class AvatarController extends AbstractController
 {
-    #[Route('/avatar', name: 'app_avatar')]
-    public function index(): Response
+    #[Route('/avatar/edit/{id}', name: 'app_avatar.edit',methods:['GET','POST'])]
+    public function edit(Avatar $avatar,Request $request,EntityManagerInterface $em,ValidatorInterface $validatorInterface,ImageService $imageService): Response
     {
+        if($this->getUser() == null){
+            return $this->redirectToRoute('app_login');
+        }
         return $this->render('avatar/index.html.twig', [
             'controller_name' => 'AvatarController',
         ]);
